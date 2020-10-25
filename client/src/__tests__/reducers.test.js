00a6ae53b0db90f2a3054ea61cb388de1c4a44a4
rhw <ruohanw89@gmail.com>
2020-10-25 04:28:52 -0700

    set up global state

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,42 @@
+// import actions
+import {
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from '../utils/actions';
+// import reducer
+import { reducer } from '../utils/reducers';
+
+// establish a sample global state
+const initialState = {
+  products: [],
+  categories: [{ name: 'Food' }],
+  currentCategory: '1',
+};
+// update state object, products
+test('UPDATE_PRODUCTS', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_PRODUCTS,
+    products: [{}, {}],
+  });
+  expect(newState.products.length).toBe(2);
+  expect(initialState.products.length).toBe(0);
+});
+// update state object, categories
+test('UPDATE_CATEGORIES', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CATEGORIES,
+    categories: [{}, {}],
+  });
+  expect(newState.categories.length).toBe(2);
+  expect(initialState.categories.length).toBe(1);
+});
+// update state object, currentCategory
+test('UPDATE_CURRENT_CATEGORY', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: '2',
+  });
+  expect(newState.currentCategory).toBe('2');
+  expect(initialState.currentCategory).toBe('1');
+});
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
+import React from 'react';
+import ProductList from '../components/ProductList';
+import CategoryMenu from '../components/CategoryMenu';
 
 const Home = () => {
-  const [currentCategory, setCategory] = useState("");
-
   return (
     <div className="container">
-      <CategoryMenu setCategory={setCategory} />
-      <ProductList currentCategory={currentCategory} />
+      <CategoryMenu />
+      <ProductList />
     </div>
   );
 };
